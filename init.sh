9276cf5727017d7cae00b5a5ec38db00e840ed9c
zhoujin <zzojin@foxmail.com>
2023-11-28 09:54:52 +0800

    NJU-ProjectN/nemu ics2023 initialized
    NJU-ProjectN/nemu eb63cf3568dbf4e0c3c6ef462e6ec685550fabbc Merge pull request #76 from rijuyuezhu/master

--- init.sh
+++ init.sh
@@ -2,10 +2,10 @@
 
 # usage: addenv env_name path
 function addenv() {
-  sed -i -e "/^export $1=.*/d" ~/.bashrc
-  echo "export $1=`readlink -e $2`" >> ~/.bashrc
-  echo "By default this script will add environment variables into ~/.bashrc."
-  echo "After that, please run 'source ~/.bashrc' to let these variables take effect."
+  sed -i -e "/^export $1=.*/d" ~/.zshrc
+  echo "export $1=`readlink -e $2`" >> ~/.zshrc
+  echo "By default this script will add environment variables into ~/.zshrc."
+  echo "After that, please run 'source ~/.zshrc' to let these variables take effect."
   echo "If you use shell other than bash, please add these environment variables manually."
 }
 
