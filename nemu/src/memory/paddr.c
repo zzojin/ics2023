1918a9d5f9de2ea496fbc02773463a6c65c34dbb
zhoujin <zzojin@foxmail.com>
2024-04-08 20:43:33 +0800

    pa 2-2：完成 memory trace

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -51,13 +51,29 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
-  IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
+    if (likely(in_pmem(addr))){
+      word_t w = pmem_read(addr, len);
+#ifdef CONFIG_MTRACE
+      Log("Read from memory at %#.8x for %d bytes: %x.", addr, len, w);
+#endif 
+      return w;
+    }
+  
+#ifdef CONFIG_DEVICE
+    word_t w = mmio_read(addr, len); 
+#ifdef CONFIG_MTRACE
+      Log("Read from memory at %#.8x for %d bytes: %x.", addr, len, w);
+#endif 
+    return w;
+#endif
   out_of_bound(addr);
   return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
+  #ifdef CONFIG_MTRACE
+      Log("Write memory at %#.8x for %d bytes: %x.", addr, len, data);
+  #endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
