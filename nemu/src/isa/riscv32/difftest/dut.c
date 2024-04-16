0a516cc8363bb094bad3ba447ead64358d2d02a4
zhoujin <zzojin@foxmail.com>
2024-04-16 19:30:47 +0800

    pa 2-2：difftest 打印格式调整，打印 pc 时输出 16 进制数

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -28,7 +28,7 @@
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
     for (int i = 0; i < 32; i++) {
         if (ref_r->gpr[i] != cpu.gpr[i]) {
-            Log("At [%u]: [%s]reg: expceted %x, but got %x\n", pc, reg_name(i), ref_r->gpr[i], cpu.gpr[i]);
+            Log("At [%x]: [%s]reg: expceted %x, but got %x\n", pc, reg_name(i), ref_r->gpr[i], cpu.gpr[i]);
             return false;
         }
     }
