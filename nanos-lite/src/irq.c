dbd5b12c2049d6b1c1bfb599fd11e2d9432abe9e
zhoujin <zzojin@foxmail.com>
2024-05-10 11:22:35 +0800

    NJU-ProjectN/navy-apps ics2023 initialized
    NJU-ProjectN/navy-apps e8346911c80af39b63e47d8aba5878e43231ad63 libos,native: add comment for the format of /proc/dispinfo

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,6 +2,7 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
+    case EVENT_YIELD: Log("event yield"); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
