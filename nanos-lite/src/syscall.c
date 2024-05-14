ae5e23bf6f587f914917c1da2107a56cd061e351
zhoujin <zzojin@foxmail.com>
2024-05-14 21:09:29 +0800

    PA3-2: 完成 strace，这个trace 工作在 nanos-lite 上，所有不能像其他 trace 在 NEMU 中设置开关，必须修改源代码 syscall.c 才行

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,17 +2,31 @@
 #include <stdint.h>
 #include "syscall.h"
 #include "am.h"
+#include "debug.h"
 
+#define CONFIG_STRACE
+// 宏定义的内部不能出现其他预处理指令, 宏不能被其他预处理指令中断
+/*#ifdef CONFIG_STRACE*/
+/*#define TRACE_CALL(func, a, ret) strace(func, a, ret)*/
+/*#else*/
+/*#define TRACE_CALL(func, a, ret)*/
+/*#endif*/
 
-#define SYSCALL_CASE(func) case SYS_##func: c->GPRx = sys_##func(a); break;
+#define SYSCALL_CASE(func) case SYS_##func: c->GPRx = sys_##func(a); strace(#func, a, c->GPRx); break;
 
-static uintptr_t sys_exit() {
+static void strace(char *s, uintptr_t *a, uintptr_t ret) {
+#ifdef CONFIG_STRACE
+    Log("[strace]%s: a0=%x a1=%x a2=%x a3=%x, ret=%d", s, a[0], a[1], a[2], a[3], ret);
+#endif
+}
+
+static uintptr_t sys_exit(uintptr_t *a) {
+    Log("[strace]exit");                    // exit 系统调用使用 halt 函数实现的时候，直接就出去了，走不到 TRACE_CALL 的逻辑，所以手动在这里添加一个日志
     halt(0);
     return 0;
 }
 
-static uintptr_t sys_yield() {
-    Log("sys yield");
+static uintptr_t sys_yield(uintptr_t *a) {
     yield();                        // yield 执行过程中会触发 ecall 指令，即系统调用内再次调用 ecall 指令
     return 0;
 }
